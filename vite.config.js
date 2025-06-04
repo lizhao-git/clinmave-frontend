33e409c8bcd7e329bde2d12c4ce88c30ed5590e2
lizhao-scholar <lizhao.scholar@gmail.com>
2025-06-04 16:40:34 +0800

    Update dependencies & settings

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      // 将所有以 /api 开头的请求代理到后端服务器
+      '/api': {
+        target: 'http://localhost:12000', // 你的后端地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // 可选：去掉 /api 前缀
+      }
+    }
+  }
 })
