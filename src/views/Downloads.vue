7410d34368c51b720a60240f34aa9f6ad9475aba
lizhao-scholar <lizhao.scholar@gmail.com>
2025-06-04 18:05:23 +0800

    Init download-page

--- src/views/Downloads.vue
+++ src/views/Downloads.vue
@@ -1,9 +1,29 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-main>
+    <v-container fluid>
+      <v-row>
+        <v-breadcumbs>
+          
+        </v-breadcumbs>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="12">
+          <v-card
+            flat
+            >
+            <v-card-title>Downloads</v-card-title>
+
+            <v-card-text>
+              <p>Download the latest version of our software:</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
+
+<script setup>
+</script>
